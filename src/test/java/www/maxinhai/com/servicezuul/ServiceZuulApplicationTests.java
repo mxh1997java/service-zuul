98799103803d3f24ce1264211af9d5b09e1ab890
15735400536 <18334698204@163.com>
2020-06-20 21:57:51 +0800

    提交代码

---
+++ src/test/java/www/maxinhai/com/servicezuul/ServiceZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package www.maxinhai.com.servicezuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ServiceZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
